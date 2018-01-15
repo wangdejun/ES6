d199c560dc621eaf1fb7fd9b207b0094218fbe25
wangdejun <1074201799@qq.com>
2018-01-15 23:59:24 +0800

    fix

--- examples/laterals.js
+++ examples/laterals.js
@@ -4,6 +4,7 @@ var human = {
         console.log('breathing...');
     }
 };
+//可以直接通过给对象的__proto__属性添加对象的方式产生继承关系；
 var worker = {
 	//设置此对象的原型为human,相当于继承human
     __proto__: human, 
--- examples/let.js
+++ examples/let.js
@@ -3,6 +3,5 @@ if(a){
 	let b = 12;
 	var c = 12;
 }
-
 console.log(c)//12
 console.log(b)//"undefined"
