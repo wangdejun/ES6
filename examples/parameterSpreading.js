441a60f012af630c1b7fc4abefd4febd8008b9cf
wangdejun <1074201799@qq.com>
2018-01-19 17:27:33 +0800

    spreading parameter in ES6

---
+++ examples/parameterSpreading.js
@@ -0,0 +1,9 @@
+function steamrollArray(arr) {
+    console.log(...arr)
+    var flat = [].concat(...arr);
+    return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+  }
+  
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+  
+  
